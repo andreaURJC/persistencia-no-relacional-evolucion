26464b3db5632ae525a34d684dfe38b126ad7dde
Juan Manuel <sanblas2b.juanma@gmail.com>
2021-02-04 18:09:52 +0100

    Initial commit

---
+++ src/main/java/com/urjc/plains-management/PlainsManagementApplication.java
@@ -0,0 +1,16 @@
+package es.urjc.code.mappedsuperclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author J. Manuel Colmenar
+ */
+@SpringBootApplication
+public class PlainsManagementApplication 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(PlainsManagementApplication.class, args);
+    }
+}
